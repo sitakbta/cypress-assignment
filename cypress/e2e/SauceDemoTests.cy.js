41abdee74dbcc1b26d40ae034121163e5630cccb
sitakbta <sitak.bta@gmail.com>
2026-09-07 21:34:26 +0100

    Updating the price for Clean Test
    Updated comment to clarify intentional failure in price assertion.

--- cypress/e2e/SauceDemoTests.cy.js
+++ cypress/e2e/SauceDemoTests.cy.js
@@ -47,7 +47,8 @@ describe('Sauce Demo Tests', () => {
       // Verify cart contains the expected item
       cartPage.verifyItemName('Sauce Labs Onesie')
       
-      // Verify the price is $7.99 as per requirement
+      // Intentional failure: site price is $7.99 but we assert $9.99 per requirement
+      // For Clean test, just changed the price now as assignment is finished and graded
       cartPage.verifyItemPrice('$7.99')
      })
 })
